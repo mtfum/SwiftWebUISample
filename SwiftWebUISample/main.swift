a866d843e3325293c3f5b3f56dd4c61bfb64e035
mtfum <f.yamanaka1127@gmail.com>
2019-12-08 17:09:38 +0900

    Initial Commit

---
+++ SwiftWebUISample/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftWebUISample
+//
+//  Created by Fumiya Yamanaka on 2019/12/08.
+//  Copyright © 2019 mtfum. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
